04826ad7dd13158fdf2313f24b9b9dd56070ace8
NatalyaMaterinskaya <natulka-91@ukr.net>
2023-11-16 02:54:00 +0200

    Updates

--- task-03.500b0e26.js
+++ task-03.500b0e26.js
@@ -587,6 +587,7 @@ let thirdCaseCounter = null;
 let fourthCaseCounter = null;
 let fifthCaseCounter = null;
 let sixthCaseCounter = null;
+let checkboxCounter = 0;
 let sevenCaseCheckbox = [];
 let sevenCaseInput = "";
 let firstCaseQuantity = null;
@@ -634,6 +635,9 @@ function clickItem(evt) {
     } else {
         btnEl.removeEventListener("click", clickItem);
         (0, _common.mainContainer).innerHTML = (0, _questions.markupForSevenBlock);
+        const submitBtnSevenBlock = document.querySelector(".answer-btn");
+        submitBtnSevenBlock.disabled = true;
+        submitBtnSevenBlock.classList.add("disabled");
         (0, _common.mainContainer).addEventListener("change", changeHandler);
         (0, _common.mainContainer).addEventListener("input", inputHandler);
         (0, _common.mainContainer).addEventListener("submit", submitHandler);
@@ -647,7 +651,6 @@ function submitHandler(evt) {
     const fourthCaseResult = Number((fourthCaseCounter / fourthCaseQuantity).toFixed(2));
     const fifthCaseResult = Number((fifthCaseCounter / fifthCaseQuantity).toFixed(2));
     const sixthCaseResult = Number((sixthCaseCounter / sixthCaseQuantity).toFixed(2));
-    console.log(typeof sixthCaseResult);
     const MPS = ((firstCaseResult + secondCaseResult + thirdCaseResult + fourthCaseResult + fifthCaseResult + sixthCaseResult) / 6).toFixed(2);
     const date = (0, _getCurrentDate.getCurrentDate)();
     const result = `<p class="result"> Результат №1 = ${firstCaseResult} </p>
@@ -657,22 +660,71 @@ function submitHandler(evt) {
   <p class="result"> Результат №5 = ${fifthCaseResult} </p>
   <p class="result"> Результат №6 = ${sixthCaseResult} </p>
   <p class="result"> Результат №7:</p>
-      <p class="result">${sevenCaseCheckbox.join("")}</p>
+      <p class="result">${sevenCaseCheckbox.join("; ")}</p>
       <p class="result">${sevenCaseInput}</p>
   <b class="result"> Основний результат = ${MPS}</b>
   <div class="date">
       <span class="time">${date.currentHours}:${date.currentMinutes}</span>
-       <spanclass="time">${date.currentDays} ${date.currentMonth} ${date.currentYear} року</span>
+       <span class="time">${date.currentDays} ${date.currentMonth} ${date.currentYear} року</span>
   </div>`;
     (0, _common.mainContainer).innerHTML = result;
 }
 function changeHandler(evt) {
+    const checkboxForm = document.querySelector(".form-question");
     const { target } = evt;
     if (target.name !== "reason") return;
-    if (target.checked === true) sevenCaseCheckbox.push(target.value);
+    //Якщо обираємо
+    if (target.checked === true) {
+        checkboxCounter += 1;
+        sevenCaseCheckbox.push(target.value);
+    }
+    //Якщо обрано 2 чекбокси, то робимо неактвиними інші + other
+    if (checkboxCounter >= 2) {
+        checkboxForm.elements.reason.forEach((element)=>{
+            if (element.checked === false) element.disabled = true;
+        });
+        checkboxForm.elements.other.disabled = true;
+        checkboxForm.elements.other.value = "";
+        sevenCaseInput = "";
+    }
+    //Якщо відміняємо
     if (target.checked === false) {
+        checkboxCounter -= 1;
         let removeItem = sevenCaseCheckbox.indexOf(target.value);
         sevenCaseCheckbox.splice(removeItem, 1);
+        //Якщо відмінили чекбокс, то робимо актвиними інші + other
+        if (checkboxCounter < 2) {
+            checkboxForm.elements.reason.forEach((element)=>{
+                if (element.checked === false) element.disabled = false;
+            });
+            checkboxForm.elements.other.disabled = false;
+        }
+    }
+    //Якщо обрано 2 чекбокси, то робимо кнопку активною
+    if (checkboxCounter === 2) {
+        checkboxForm.lastElementChild.disabled = false;
+        checkboxForm.lastElementChild.classList.remove("disabled");
+    }
+    //Якщо обрано 1 чекбокс, a other пустий, то робимо кнопку неактивною
+    if (checkboxCounter === 1 && sevenCaseInput === "") {
+        checkboxForm.lastElementChild.disabled = true;
+        checkboxForm.lastElementChild.classList.add("disabled");
+    }
+    //Якщо спочатку обрано 1 чекбокс, a other не пустий,то робимо кнопку активною
+    checkboxForm.elements.other.addEventListener("blur", ()=>{
+        if (checkboxCounter === 1 && sevenCaseInput !== "") {
+            checkboxForm.lastElementChild.disabled = false;
+            checkboxForm.lastElementChild.classList.remove("disabled");
+        }
+    });
+    //Якщо спочатку заповнено other, а потім обрано 1 чекбокс, то робимо кнопку активною
+    if (checkboxCounter === 1 && sevenCaseInput !== "") {
+        checkboxForm.lastElementChild.disabled = false;
+        checkboxForm.lastElementChild.classList.remove("disabled");
+    }
+    if (checkboxCounter === 0 && sevenCaseInput !== "") {
+        checkboxForm.lastElementChild.disabled = true;
+        checkboxForm.lastElementChild.classList.add("disabled");
     }
 }
 function inputHandler(evt) {
